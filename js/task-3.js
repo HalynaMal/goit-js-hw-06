eede875bce0b9c66135758d4f1e92ac436a910f2
Halyna <dguoit@ukr.net>
2023-12-12 14:40:45 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,26 @@
 class StringBuilder {
-    value;
+    #value;
     constructor(initialValue = '') {
-        this.value = initialValue;
+        this.#value = initialValue;
     }
 
     //повертає поточне значення приватної властивості value.
     getValue() {
-        return this.value;
+        return this.#value;
     }  
 // отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод
     padEnd(str) {
-        this.value += str;
+        this.#value += str;
     }
 
 // отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод
     padStart(str) { 
-        this.value = str + this.value;
+        this.#value = str + this.#value;
     }
 
 // отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості  value об'єкта, який викликає цей метод    
     padBoth(str) { 
-        this.value = str + this.value + str;
+        this.#value = str + this.#value + str;
     }
 
 }
